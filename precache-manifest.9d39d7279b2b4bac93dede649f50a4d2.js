36a1f9e8d97ff83ea7d80e785f55331a986a732c
NikitaUshenko <nikita-ushenko@mail.ru>
2019-08-16 14:00:02 +0300

    Updates

---
+++ precache-manifest.9d39d7279b2b4bac93dede649f50a4d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e986f4c3a37554593df6677e6c5d8864",
+    "url": "/test_task_openGeeksLab/index.html"
+  },
+  {
+    "revision": "eb71046785af3a7ceca8",
+    "url": "/test_task_openGeeksLab/static/css/main.67a63642.chunk.css"
+  },
+  {
+    "revision": "558185ffd5f7bf97d280",
+    "url": "/test_task_openGeeksLab/static/js/2.8662aadb.chunk.js"
+  },
+  {
+    "revision": "eb71046785af3a7ceca8",
+    "url": "/test_task_openGeeksLab/static/js/main.fbdeb5b1.chunk.js"
+  },
+  {
+    "revision": "a4c7b959eb6cccc9e82a",
+    "url": "/test_task_openGeeksLab/static/js/runtime~main.0523aeff.js"
+  }
+]);
